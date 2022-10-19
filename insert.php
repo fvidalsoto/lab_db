3a8cf102530d5491dc39f255bbe40f7b0339cfa9
Felipe Andres Vidal Soto <felipeavidalsoto@gmail.com>
2022-10-19 14:54:57 -0300

    FIX: Se corrige variable

--- insert.php
+++ insert.php
@@ -2,26 +2,26 @@
 require('./conexion.php');
 
 /* SCRIPT MySQL */
-$contador = 0;
+$j = 0;
 for ($i = 0; $i < 1000000; ++$i) {
     $texto = 'Provincia-' . $i;
     $consulta = "INSERT INTO provincia (nombre) VALUES ('$texto')";
     $result =  $mysqli->query($consulta);
-    $contador++;
+    $j++;
 }
 
-echo $contador;
+echo $j;
 
 /* SCRIPT PostgreSQL */
 /* 
-$contador = 0;
+$j = 0;
 
 for ($i = 0; $i < 1000000; ++$i) {
     $texto = 'Provincia-' . $i;
     $consulta = "INSERT INTO gestion_instalaciones.provincia (nombre) VALUES ('$texto');";
     $result =  pg_query($consulta) or die('La consulta fallo: ' . pg_last_error());
-    $contador++;
+    $j++;
 }
 
-echo $contador;
+echo $j;
  */
